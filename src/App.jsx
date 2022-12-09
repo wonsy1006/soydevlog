2e0e7a47f1716ae699dce4154fdb1be1321dfc26
wonsy1006 <wonsy1006@gmail.com>
2022-12-09 23:15:55 +0900

    - tailwind.config.js 적용되는지 확인

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
-      <p>Hello world!</p>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p className="bg-purple-700">This is the way</p>
     </div>
   );
 }
